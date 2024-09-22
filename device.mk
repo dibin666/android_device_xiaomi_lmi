4bac33e133355e3fcf6ba060e8f9c22f516b0bae
dibin666 <ddddd2468hjk@gmail.com>
2024-09-22 13:57:20 +0800

    Support Alipay fingerprint payment

--- device.mk
+++ device.mk
@@ -45,7 +45,9 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.audio.zoom.support=true \
     ro.vendor.audio.zoom.type=1 \
     vendor.audio.spkcal.copy.inhal=true \
-    vendor.audio.usb.disable.sidetone=true
+    vendor.audio.usb.disable.sidetone=trueI \
+    ro.hardware.fingerprint=fpc \
+    persist.vendor.sys.fp.vendor=fpc
 
 PRODUCT_ODM_PROPERTIES += \
     aaudio.mmap_policy=1 \
